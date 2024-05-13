e2886673a83ed5bbe9ea8e92c97b1fb171f720f1
Latte39777 <s1f102400549@iniad.org>
2024-05-13 23:08:59 +0900

    数字表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,70 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
-function getRandomCoordinates(): [number, number] {
-  const xs = Math.floor(Math.random() * 9); // 0から8までのランダムな数
-  const ys = Math.floor(Math.random() * 9); // 0から8までのランダムな数
+const directions = [
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+];
 
+function getRandomCoordinates(): [number, number] {
+  const xs = Math.floor(Math.random() * 9);
+  const ys = Math.floor(Math.random() * 9);
   return [ys, xs];
 }
 
-const bombPlace = (x: number, y: number, bombMap: number[][]) => {
-  const getBombPlace = [];
-  while (getBombPlace.length < 10) {
-    const [ys, xs] = getRandomCoordinates();
-    const exists = getBombPlace.some(([yb, xb]) => xb === xs && yb === ys);
-    if (!exists && x !== xs && y !== ys) {
-      if (x !== xs && y !== ys) {
-        getBombPlace.push([ys, xs]);
-        bombMap[ys][xs] = -1;
+const numberMap = (bombMap: number[][]) => {
+  let count = 0;
+  for (let a = 0; a < 9; a++) {
+    for (let b = 0; b < 9; b++) {
+      for (const direction of directions) {
+        const dx = direction[0];
+        const dy = direction[1];
+        if (
+          bombMap[b + direction[1]] !== undefined &&
+          bombMap[b + dy][a + dx] === -1 &&
+          bombMap[b][a] !== -1
+        ) {
+          console.log('A', a, 'B', b, count);
+          count++;
+          continue;
+        }
+      }
+      if (bombMap[b][a] !== -1) {
+        bombMap[b][a] = count;
+        count = 0;
       }
     }
   }
   return bombMap;
 };
 
-const Home = () => {
-  // const [samplePos, setSamplePos] = useState(0);
-  // console.log(samplePos);
-  // const [representBoard, bombMap] = useState([
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  // ]);
+const bombPlace = (x: number, y: number, bombMap: number[][]) => {
+  const getBombPlace: [number, number][] = [];
+  while (getBombPlace.length < 10) {
+    const [ys, xs] = getRandomCoordinates();
+    let exists = false;
+    for (const [yb, xb] of getBombPlace) {
+      if (xb === xs && yb === ys) {
+        exists = true;
+        break;
+      }
+    }
+    if (!exists && x !== xs && y !== ys) {
+      console.log('ボマー', xs, ys);
+      getBombPlace.push([ys, xs]);
+      bombMap[ys][xs] = -1;
+    }
+  }
+  return bombMap;
+};
 
+const Home = () => {
   const [bombMap, userInput] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -53,7 +80,9 @@ const Home = () => {
   const clickHandler = (x: number, y: number) => {
     const newBoard = structuredClone(bombMap);
     const newbombPlaed = bombPlace(x, y, newBoard);
-    userInput(newbombPlaed);
+    const newnumber = numberMap(newbombPlaed);
+    userInput(newnumber);
+    // userInput(newbombPlaed);
   };
 
   return (
